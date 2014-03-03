65896518a777b7e242abb0d2cedc705e0d93b79d
kolektiv <andrew@xyncro.com>
2014-03-03 19:26:00 +0000

    initial

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var stat = require('node-static');
+
+var components = [
+	{ name: 'core', port: 8081 },
+	{ name: 'menu', port: 8082 }
+];
+
+components.forEach(function (c) {
+	var server = new stat.Server('./components/' + c.name, { cache: false });
+
+	http.createServer(function (req, res) {
+		req.addListener('end', function () {
+			server.serve(req, res);
+		}).resume();
+	}).listen(c.port);
+	
+	console.log('server ' + c.name + ' running on ' + c.port);
+});
+
